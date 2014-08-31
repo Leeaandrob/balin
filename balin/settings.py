c6e5ee6e20a16c975934c1dc41951720131d0372
gilsondev <contato@gilsondev.com>
2014-08-31 12:34:43 -0300

    Definindo S3

--- balin/settings.py
+++ balin/settings.py
@@ -55,6 +55,7 @@ THIRD_PARTY_APPS = (
     'widget_tweaks',
     'djangojs',
     'eztables',
+    'collectfast',
 )
 
 LOCAL_APPS = (
@@ -130,6 +131,23 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, 'static'),
 )
+
+if DEBUG is True:
+    DEFAULT_FILE_STORAGE = 'balin.s3utils.MediaRootS3BotoStorage'
+    STATICFILES_STORAGE = 'balin.s3utils.StaticRootS3BotoStorage'
+
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKET_NAME', '')
+    AWS_QUERYSTRING_AUTH = False
+    AWS_PRELOAD_METADATA = True
+    AWS_HEADERS = {
+        'Expires': 'Thu, 15 Apr 2010 20:00:00 GMT',
+        'Cache-Control': 'max-age=86400',
+    }
+
+    MEDIA_URL = 'https://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
+    STATIC_URL = 'https://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
 
 # Auth Configuration
 LOGIN_REDIRECT_URL = '/'
